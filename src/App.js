5d223fe514b975a9bd6d34ee2a2e0d0e0301f416
Bartek <bartek.kryczka@wp.pl>
2023-07-18 22:13:56 +0200

    added styling to dashboard

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import { Fragment } from 'react';
-import GradeList from './components/GradeList';
+import './App.css'
+import { Fragment } from 'react'
+import GradeList from './components/GradeList'
 import classes from './App.module.css'
 
 const App = () => {
-
-  return (
-	<Fragment >
-		<main className={classes.wrapper}>
-    	<h1>Podaj maksymalną liczbę punktów</h1>
-    	<input type="number"></input>
-    	<button>Oblicz!</button>
-		<GradeList></GradeList>
-		</main>
-	</Fragment>
-  );
+	return (
+		<Fragment>
+			<main className={classes.wrapper}>
+				<h1 className={classes.title}>Podaj maksymalną liczbę punktów</h1>
+				<input className={classes.input} type='number'></input>
+				<button className={classes.button}>Oblicz!</button>
+				<GradeList></GradeList>
+			</main>
+		</Fragment>
+	)
 }
 
-export default App;
+export default App
